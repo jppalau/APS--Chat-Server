39bcf5467b8318ec8a627a7c2d425b78c90bc8d6
jppalau <jpfp2703@gmail.com>
2022-04-02 18:39:42 -0300

    uptade name Chat

--- Faculdade/APS5/ChatServer/src/main/java/com/project/chatserver/Chat.java
+++ Faculdade/APS5/ChatServer/src/main/java/com/project/chatserver/Chat.java
@@ -13,57 +13,54 @@ import java.util.Scanner;
  *
  * @author ZF676VA
  */
-public class Chat implements Runnable{
+public class Chat implements Runnable {
+
     public static final String SERVER_ADDRESS = "127.0.0.1";
-    
+
     private ClientSocket clientSocket;
     public String msg;
-    public static String nome = "vleber";
-    
-    
+    public static String nome = "Jorge";
+
     public static void main(String[] args) {
         try {
             Chat chat = new Chat();
             chat.start();
         } catch (IOException e) {
             System.out.println("Erro ao conectar ao servidor: " + e.getMessage());
-        }     
+        }
     }
-    
+
     /*public Chat(){
         scr = new Scanner(System.in);
     }*/
-    
-    public void start() throws IOException{
+    public void start() throws IOException {
         final Socket socket = new Socket(SERVER_ADDRESS, Server.PORT);
         clientSocket = new ClientSocket(socket, nome);
         System.out.println("Cliente conectado ao servidor no endereço " + SERVER_ADDRESS + " e porta " + Server.PORT);
         System.out.println("Digite uma msg (ou 'sair' para encerrar): ");
-        
+
         Thread thread = new Thread(this);
         thread.start();
-        
+
         messageLoop();
-        
+
     }
-    
+
     @Override
     public void run() {
-        while((msg = clientSocket.getMessage())!= null) {
+        while ((msg = clientSocket.getMessage()) != null) {
             System.out.println(msg);
         }
     }
-    
-    public void messageLoop(){
-        do {     
+
+    public void messageLoop() {
+        do {
             Scanner scr = new Scanner(System.in);
             msg = scr.nextLine();
             clientSocket.sendMsg(msg);
         } while (!"sair".equalsIgnoreCase(msg));
-            clientSocket.close();
+        clientSocket.close();
 
     }
-    
-    
-    
+
 }
